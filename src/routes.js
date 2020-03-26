42e636b87562e6c60c95cdede2b0d23cba065e08
brunubarbosa <brunopjsw@gmail.com>
2020-03-26 04:37:31 -0300

    done

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,15 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection');
-
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 const routes = express.Router();
 
-routes.post('/ongs', async (req, res) => {
-  const { name, email, whatsapp, city, uf } = req.body;
-  const id = crypto.randomBytes(4).toString('HEX');
-
-  await connection('ongs').insert({
-    id,
-    name,
-    email,
-    whatsapp,
-    city,
-    uf,
-  })
-  return res.json({id});
-})
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create)
+routes.post('/incidents', IncidentController.create)
+routes.get('/incidents', IncidentController.index)
+routes.delete('/incidents/:id', IncidentController.delete)
+routes.get('/profile', ProfileController.index)
+routes.post('/login', SessionController.create)
 module.exports = routes;
